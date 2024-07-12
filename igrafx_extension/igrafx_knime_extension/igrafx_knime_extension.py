c55fcb3ae6be373c29d80f7de0848475675c53bd
Dourouve <dhruv.maulloo@igrafx.com>
2024-07-12 10:01:40 +0200

    feat: added max mine length

--- igrafx_extension/igrafx_knime_extension/igrafx_knime_extension.py
+++ igrafx_extension/igrafx_knime_extension/igrafx_knime_extension.py
@@ -704,9 +704,7 @@ class iGrafxProjectDataNode:
 
         # Check and replace the column name if it matches the pattern "case_+databasecolumnname"
         df.rename(columns=lambda col: f"{column_name_mapping_infos[col[5:]]} (case)" if col.startswith("case_") and
-                                                                                        col[
-                                                                                        5:] in column_name_mapping_infos else col,
-                  inplace=True)
+                  col[5:] in column_name_mapping_infos else col,inplace=True)
 
         # Filter columns based on the condition
         columns_to_keep = [col for col in df.columns if all(keyword not in col for keyword in
